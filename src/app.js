03e4dc3f3f25d8e77652dcdfaac1b44796588986
Meganide <renashassan@hotmail.com>
2023-04-03 10:28:20 +0200

    import extension off eslint

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
-import api from './router/api';
+import api from './router/api.js';
+import connectDB from './config/db.js';
 
 const app = express();
+
+connectDB();
 
 app.use('/api', api);
 
