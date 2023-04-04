3458e892ea42eb26d86619ecf6a729ebee1680ed
Jonas Larsson <larsson2p0@MacBook-Pro-som-tillhor-Jonas.local>
2023-04-04 10:35:59 +0200

    remove bodyParser, using express.json

--- src/app.js
+++ src/app.js
@@ -1,12 +1,8 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import api from './router/api.js';
 import connectDB from './config/db.js';
 
 const app = express();
-
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 connectDB();
 
