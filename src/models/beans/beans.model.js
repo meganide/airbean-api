e09edad3b4235c1499e050e39c9c3f79c60679a8
Meganide <renashassan@hotmail.com>
2023-04-06 11:42:02 +0200

    added getorder route

--- src/models/beans/beans.model.js
+++ src/models/beans/beans.model.js
@@ -1,3 +1,7 @@
+import add from "date-fns/add/index.js";
+import { parseISO } from "date-fns";
+import differenceInMinutes from "date-fns/differenceInMinutes/index.js";
+
 import { Order } from "./beans.schema.js";
 
 async function createOrder(orderDetails, userId) {
@@ -14,5 +18,15 @@ async function createOrder(orderDetails, userId) {
   return newOrder;
 }
 
+async function getEtaByOrderNr(orderNr) {
+  const { eta, createdAt } = await Order.findOne({ orderNr }).select("eta createdAt");
+
+  const expectedDeliveryISO = add(parseISO(createdAt), { minutes: eta });
+  const currentDate = new Date();
+  const currentEta = differenceInMinutes(expectedDeliveryISO, currentDate, { roundingMethod: "ceil" });
+
+  return currentEta;
+}
+
 // eslint-disable-next-line import/prefer-default-export
-export { createOrder };
+export { createOrder, getEtaByOrderNr };
--- src/router/beans.router.js
+++ src/router/beans.router.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { httpCreateOrder, httpGetMenu } from "../controllers/beans.controller.js";
+import { httpCreateOrder, httpGetEtaByOrderNr, httpGetMenu } from "../controllers/beans.controller.js";
 import { validateOrder } from "../middleware/beans.middleware.js";
 import validateToken from "../middleware/token.middleware.js";
 
 const beansRouter = express.Router();
 
 beansRouter.get("/", httpGetMenu);
+beansRouter.get("/order/status/:orderNr", validateToken, httpGetEtaByOrderNr);
 beansRouter.post("/order", validateToken, validateOrder, httpCreateOrder);
 
 export default beansRouter;
